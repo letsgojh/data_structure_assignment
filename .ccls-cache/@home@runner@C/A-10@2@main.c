2a7078db20699f8c3c8d2d577d1bd2c1cbdc5e10
Yu-Jaehwan <138412939+letsgojh@users.noreply.github.com>
2024-06-13 11:34:58 +0000

    create A-11 1

--- .ccls-cache/@home@runner@C/A-10@2@main.c
+++ .ccls-cache/@home@runner@C/A-10@2@main.c
@@ -1,3 +1,4 @@
+/*
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,92 +12,87 @@ struct node {
   char data;
 };
 
-node * root = NULL;
+node *root = NULL;
 int front = 0;
 int rear = 0;
 
 node *queue[MAX_QUEUE_SIZE];
 
-
-void inOrder(node ** root);
-void preOrder(node ** root);
-void postOrder(node ** root);
-void addNode(node ** root,char data);
+void inOrder(node **root);
+void preOrder(node **root);
+void postOrder(node **root);
+void addNode(node **root, char data);
 
 void Input();
 
 int main(void) { Input(); }
 
-void inOrder(node ** root){
+void inOrder(node **root) {
 
-  node * currentNode = *root;
+  node *currentNode = *root;
 
-  if(currentNode->left != NULL)
+  if (currentNode->left != NULL)
     inOrder(&currentNode->left);
-  
-  printf("%c ",currentNode->data);
 
-  if(currentNode->right != NULL)
+  printf("%c ", currentNode->data);
+
+  if (currentNode->right != NULL)
     inOrder(&currentNode->right);
-
 }
-void preOrder(node ** root){
-  node * currentNode = *root;
+void preOrder(node **root) {
+  node *currentNode = *root;
 
-  printf("%c ",currentNode->data);
-  
-  if(currentNode->left != NULL)
+  printf("%c ", currentNode->data);
+
+  if (currentNode->left != NULL)
     preOrder(&currentNode->left);
 
-  if(currentNode->right != NULL)
+  if (currentNode->right != NULL)
     preOrder(&currentNode->right);
 }
-void postOrder(node ** root){
-  node * currentNode = *root;
+void postOrder(node **root) {
+  node *currentNode = *root;
 
-
-  if(currentNode->left != NULL)
+  if (currentNode->left != NULL)
     preOrder(&currentNode->left);
 
-  if(currentNode->right != NULL)
+  if (currentNode->right != NULL)
     preOrder(&currentNode->right);
 
-  printf("%c ",currentNode->data);
+  printf("%c ", currentNode->data);
 }
 
-void addNode(node ** root,char data) {
+void addNode(node **root, char data) {
 
   node *newNode = (node *)malloc(sizeof(node));
   newNode->left = NULL;
   newNode->right = NULL;
   newNode->data = data;
 
-
   if (rear == 0) {
     queue[rear++] = newNode;
     *root = newNode;
     return;
   }
-  
 
-  while(1){ // queue에 char 데이터만 따로 저장하려고 했는데 되지 않는다.. 왜지?
-  
+  while (
+      1) { // queue에 char 데이터만 따로 저장하려고 했는데 되지 않는다.. 왜지?
+
     if (queue[front]->left == NULL) {
       queue[front]->left = newNode;
       queue[rear++] = newNode;
-      //printf("case1\n");
+      // printf("case1\n");
       break;
     }
-  
+
     else if (queue[front]->right == NULL) {
       queue[front]->right = newNode;
       queue[rear++] = newNode;
-      //printf("case2\n");
+      // printf("case2\n");
       break;
     }
 
     front++;
-    
   }
 }
 
@@ -111,17 +107,16 @@ void Input() {
   }
 
   char k;
-  node * root = NULL;
+  node *root = NULL;
 
-  
   while (1) {
     k = fgetc(f);
-    if(k == EOF) //문자 한개 받을때는 이런식으로 받기 , NULL값까지 받으니깐 트리 이상하게 만들어진다.
+    if (k == EOF) //문자 한개 받을때는 이런식으로 받기 , NULL값까지 받으니깐
+                  //트리 이상하게 만들어진다.
       break;
-    //printf("%c",k);
-    addNode(&root,k);
+    // printf("%c",k);
+    addNode(&root, k);
   }
-
 
   printf("creating a complete binary tree\n\n");
   printf("three binary tree traversals\n");
@@ -131,4 +126,4 @@ void Input() {
   preOrder(&root);
   printf("\npostorder traversal     : ");
   postOrder(&root);
-}
+}*/
